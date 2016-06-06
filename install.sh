8bdfb11dd6d05c15769599bcb0f14f1a654d8fdc
alex-estela <alex@estela.fr>
2016-06-06 21:50:14 +0200

    Fix RPI deployment

--- install.sh
+++ install.sh
@@ -12,6 +12,13 @@ sudo apt-get update
 
 sudo apt-get install -y tint2 matchbox-window-manager postgresql imagemagick
 
+wget http://www-us.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
+sudo tar -xzvf apache-maven-3.3.9-bin.tar.gz
+echo 'export M2_HOME=/home/pi/apache-maven-3.3.9
+export PATH=$PATH:$M2_HOME/bin ' >> /home/pi/maven.sh
+sudo mv /home/pi/maven.sh /etc/profile.d/maven.sh
+source /etc/profile.d/maven.sh
+
 git clone git://github.com/rg3/youtube-dl
 sudo ln -s ~/youtube-dl/youtube_dl/__main__.py /usr/bin/youtube-dl
 
@@ -34,20 +41,11 @@ sudo sed -i 's/allowed_users=console/allowed_users=anybody/g' /etc/X11/Xwrapper.
 # Config spéciale HDML PI 7
 sudo sed -i '/README/alcd_rotate=2' /boot/config.txt
 
-wget http://www-us.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
-sudo tar -xzvf apache-maven-3.3.9-bin.tar.gz
-
-echo 'export M2_HOME=/home/pi/apache-maven-3.3.9
-export PATH=$PATH:$M2_HOME/bin ' >> /home/pi/maven.sh
-sudo mv /home/pi/maven.sh /etc/profile.d/maven.sh
-. /etc/profile.d/maven.sh
-
+sudo ln -s /usr/bin/convert /usr/bin/convert-piframe
 
 cd /home/pi/piframe
 mkdir tmp
 mvn clean install spring-boot:repackage
-
-sudo ln -s /usr/bin/convert /usr/bin/convert-piframe
 
 echo '#! /bin/sh
 
@@ -81,7 +79,6 @@ case "$1" in
 esac
 
 exit 0' >> /home/pi/initd-piframe
-
 sudo mv /home/pi/initd-piframe /etc/init.d/piframe
 sudo chmod +x /etc/init.d/piframe
 sudo update-rc.d piframe defaults
@@ -89,13 +86,10 @@ sudo update-rc.d piframe defaults
 sudo runuser -l postgres -c "psql -c \"CREATE USER piframe WITH PASSWORD 'piframe';\""
 sudo runuser -l postgres -c "psql -c \"CREATE DATABASE piframe OWNER piframe;\""
 
-
 sudo raspi-config --expand-rootfs
 
 # enable console auto login (emulate raspi-config boot behaviour B2)
-
 sudo systemctl set-default multi-user.target
 sudo ln -fs /etc/systemd/system/autologin@.service /etc/systemd/system/getty.target.wants/getty@tty1.service
-
 
 sudo reboot
